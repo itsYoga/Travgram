cb80ce10a5d3b8c6920fc32600e7f64f1a510a6f
YuJiaLiang0702 <ch993115@gmail.com>
2024-12-23 16:50:41 +0800

    Initial Commit

---
+++ Travgram/TravgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravgramApp.swift
+//  Travgram
+//
+//  Created by Jesse Liang on 2024/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TravgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
